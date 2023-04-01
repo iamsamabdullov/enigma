4ad0c9a5d4092b7792f41e7231d3c5228257b1c7
iamsamabdullov <107039382+iamsamabdullov@users.noreply.github.com>
2023-04-01 20:56:40 +0300

    Create enigma.py

---
+++ en/enigma.py
@@ -0,0 +1,31 @@
+def enigma(text, ref, rot1, rot2, rot3):
+
+    alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    rot1 =     'EKMFLGDQVZNTOWYHXUSPAIBRCJ'
+    if rot2 == 2:
+        rot2 = 'AJDKSIRUXBLHWTMCQGZNPYFVOE'
+    else:
+        rot2 = alphabet
+    if rot3 == 3:
+        rot3 = 'BDFHJLCPRTXVZNYEIWGAKMUSQO'
+    else:
+        rot3 = alphabet
+    ref = 'YRUHQSLDPXNGOKMIEBFZCWVJAT'
+
+    txt = ''.join([t for t in text.upper() if t in alphabet])
+    trantab = txt.maketrans(alphabet, rot3)
+    x = str(txt.translate(trantab))
+    trantab1 = x.maketrans(alphabet, rot2)
+    x1 = x.translate(trantab1)
+    trantab2 = x1.maketrans(alphabet,rot1)
+    x2 = x1.translate(trantab2)
+    trantab3 = x2.maketrans(alphabet,ref)
+    x3 = x2.translate(trantab3)
+    trantab4 = x3.maketrans(rot1, alphabet)
+    x4 = x3.translate(trantab4)
+    trantab5 = x4.maketrans(rot2, alphabet)
+    x5 = x4.translate(trantab5)
+    trantab6 = x5.maketrans(rot3, alphabet)
+    x6 = x5.translate(trantab6)
+
+    return x6
